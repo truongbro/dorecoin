ba3b74ef443fc80dd42671ce244ca7d731313823
truongbro <107202848+truongbro@users.noreply.github.com>
2023-04-09 03:44:05 -0500

    Delete rpc_mining.sh

--- rpc_mining.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
-while :; do
-    ./astrominer -w dero1qykhjmvugtgvnm7xre6a6ayj77806qul669ydknka45nkck9xjg37qgl0pa99 -r community-pools.mysrv.cloud:10300 -p rpc;
-    sleep 5;
-done
